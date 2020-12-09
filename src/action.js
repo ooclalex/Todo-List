cf82125b25fd2e0c3ae1b8f0c5d5e7b6273562ce
Alex Liu <liual@oocl.com>
2020-12-09 15:22:18 +0800

    [todo][alex liu]feat: init todo

--- src/action.js
+++ src/action.js
@@ -1,4 +1,4 @@
-import { ADD_TODO_ITEM, DELETE_TODO_ITEM, UPDATE_TODO_ITEM } from "./actionTypes";
+import { ADD_TODO_ITEM, DELETE_TODO_ITEM, UPDATE_TODO_ITEM, INIT_TODOS } from "./actionTypes";
 
 export const addTodoItem = (text) => (
     {type: ADD_TODO_ITEM, payload: text}
@@ -11,3 +11,8 @@ export const updateTodoItem = (id) => (
 export const deleteTodoItem = (id) => (
     {type: DELETE_TODO_ITEM, payload: id}
 )
+
+export const initTodos = (todos) => ({
+    type: INIT_TODOS,
+    payload:  todos
+})
