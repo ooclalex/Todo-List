7743f54edcdb7a4db96f3f7002062d00f30eccc9
Alex Liu <liual@oocl.com>
2020-12-10 10:54:46 +0800

    [todo][alex liu]refactor: reducer change if to switch

--- src/reducer.js
+++ src/reducer.js
@@ -8,40 +8,36 @@ import {
 } from "./actionTypes";
 
 const todoItemList = (state = [], action) => {
-  // TODO change to switch
-  // redux toolkit
-  if (action.type === ADD_TODO_ITEM) {
-    return [...state, action.payload];
-  }
-  if (action.type === UPDATE_TODO_ITEM) {
-    return state.map((todo) => {
-      if (todo.id === action.payload) {
-        return {
-          ...todo,
-          done: !todo.done,
-        };
-      }
-      return todo;
+  switch (action.type) {
+    case ADD_TODO_ITEM:
+      return [...state, action.payload];
+    case UPDATE_TODO_ITEM: 
+      return state.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            done: !todo.done,
+          };
+        }
+        return todo;
     });
+    case DELETE_TODO_ITEM:
+      return state.filter((item) => item.id !== action.payload);
+    case INIT_TODOS:
+      return action.payload;
+    case ADD_TAG:
+      return state.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return {
+            ...todo,
+            tag: [...todo.tag, action.payload.tag],
+          };
+        }
+        return todo;
+      });
+    default:
+      return state;
   }
-  if (action.type === DELETE_TODO_ITEM) {
-    return state.filter((item) => item.id !== action.payload);
-  }
-  if (action.type === INIT_TODOS) {
-    return action.payload;
-  }
-  if (action.type === ADD_TAG) {
-    return state.map((todo) => {
-      if (todo.id === action.payload.id) {
-        return {
-          ...todo,
-          tag: [...todo.tag, action.payload.tag],
-        };
-      }
-      return todo;
-    });
-  }
-  return state;
 };
 
 export default combineReducers({
